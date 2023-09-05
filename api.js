7c4380f85e91b173266d2890c7254f78ef2edfb7
A-E-Harding <aeg.h@btinternet.com>
2023-09-05 16:40:32 +0100

    completing task 7

--- api.js
+++ api.js
@@ -1,15 +1,23 @@
 import axios from "axios";
 
-const baseURL = "https://nc-news-0876.onrender.com/api"
+const baseURL = "https://nc-news-0876.onrender.com/api";
 
 export function getAllArticles() {
-    return axios.get(baseURL+'/articles')
+  return axios.get(baseURL + "/articles");
 }
 
 export function getArticleById(article_id) {
-    return axios.get(baseURL+`/articles/${article_id}`)
+  return axios.get(baseURL + `/articles/${article_id}`);
 }
 
 export function getCommentsByArticleId(article_id) {
-    return axios.get(baseURL+`/articles/${article_id}/comments`)
+  return axios.get(baseURL + `/articles/${article_id}/comments`);
 }
+
+export function updateVotes(article_id) {
+  return axios.patch(baseURL + `/articles/${article_id}`, { inc_votes: 1 });
+}
+
+export function decreaseVotes(article_id) {
+    return axios.patch(baseURL + `/articles/${article_id}`, { inc_votes: -1 });
+  }
