06c5b297b726eabe8f4b0ee5041f13d7223c7118
A-E-Harding <aeg.h@btinternet.com>
2023-09-08 10:17:16 +0100

    task 10-sort articles

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const baseURL ="https://nc-news-0876.onrender.com/api"
-  //"http://localhost:9090/api"
+const baseURL = "https://nc-news-0876.onrender.com/api"
+//"http://localhost:9090/api"
+
 
 
 export function getAllArticles() {
@@ -16,22 +17,24 @@ export function getCommentsByArticleId(article_id) {
   return axios.get(baseURL + `/articles/${article_id}/comments`);
 }
 
-
 export function updateVotes(article_id) {
   return axios.patch(baseURL + `/articles/${article_id}`, { inc_votes: 1 });
 }
 export function decreaseVotes(article_id) {
-    return axios.patch(baseURL + `/articles/${article_id}`, { inc_votes: -1 });
-  }
+  return axios.patch(baseURL + `/articles/${article_id}`, { inc_votes: -1 });
+}
 
 export function addComment(comment, article_id) {
-     return axios.post(baseURL+`/articles/${article_id}/comments`, {username: "grumpy19", body:comment})
-}
- 
-export function getAllTopics() {
-  return axios.get(baseURL+"/topics")
+  return axios.post(baseURL + `/articles/${article_id}/comments`, {
+    username: "grumpy19",
+    body: comment,
+  });
 }
 
-export function getArticlesByTopic(topicName) {
-  return axios.get(baseURL+`/articles?topic=${topicName}`)
+export function getAllTopics() {
+  return axios.get(baseURL + "/topics");
+}
+
+export function getArticlesByTopic(topicName, sortBy, orderBy) {
+  return axios.get(baseURL + `/articles?topic=${topicName}&sort_by=${sortBy}&order=${orderBy}`);
 }
--- src/components/TopicPage.jsx
+++ src/components/TopicPage.jsx
@@ -1,30 +1,59 @@
 import { useParams } from "react-router-dom";
-import { getArticlesByTopic } from "../../api"
+import { getArticlesByTopic } from "../../api";
 import { Link } from "react-router-dom";
-import {useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function TopicPage({ articles, setArticles }) {
-  
   const [searchParams, setSearchParams] = useSearchParams();
-  const topicName = searchParams.get('topic');
+  const [sortedBy, setSortedBy] = useState("created_at");
+  const [orderedBy, setOrderedBy] = useState("desc");
 
-  
-  getArticlesByTopic(topicName).then((response) => {
-        setArticles(response.data)
-    })
-    return (
-        <main className="all-articles">
-            <h2 className='topic-header'>{topicName}</h2>
+  const topicName = searchParams.get("topic");
+
+  const handleSortClick = (sortBy) => {
+    setSortedBy(sortBy);
+  };
+
+  const handleOrderClick = (orderBy) => {
+    setOrderedBy(orderBy);
+  };
+
+  getArticlesByTopic(topicName, sortedBy, orderedBy).then((response) => {
+    setArticles(response.data);
+  });
+
+  return (
+    <main className="all-articles">
+      <h2 className="topic-header">{topicName}</h2>
+      <div className="asc-desc">
+        <button onClick={() => handleOrderClick("asc")}>ascending</button>
+        <button onClick={() => handleOrderClick("desc")}>descending</button>
+      </div>
+      <div className="dropdown">
+        <button className="dropbtn">sort by...</button>
+        <div className="dropdown-content">
+          <button onClick={() => handleSortClick("votes")}>Votes</button>
+          <button onClick={() => handleSortClick("comment_count")}>
+            Comments
+          </button>
+          <button onClick={() => handleSortClick("created_at")}>Date</button>
+        </div>
+      </div>
       {articles.map((article) => {
         return (
-          <div key={article.article_id}>
+          <div className="article-card" key={article.article_id}>
             <Link to={`/articles/${article.article_id}`}>
-              <p >{article.title}</p>
+              <p>{article.title}</p>
               <p>{article.author}</p>
+              <p>{article.created_at}</p>
+              <p>votes {article.votes}</p>
+              <p>comments {article.comment_count}</p>
             </Link>
           </div>
         );
       })}
     </main>
-    )
+  );
 }
